42f12b17d33efd89cbccbb1c459ba37b70e7c249
Calvin So <ccs403@nyu.edu>
2016-09-12 15:06:01 -0400

    Saving work before merge

---
+++ Assets/FearContributor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FearContributor : MonoBehaviour {
+
+    [SerializeField]
+    Transform child;
+
+    private float fearLevel, timer;
+    [SerializeField]
+    float fearGain;
+    [SerializeField]
+    float fearGainRate;
+    [SerializeField]
+    float shakeThreshold;
+    [SerializeField]
+    float shakeSpeed;
+
+	// Use this for initialization
+	void Start () {
+        child = GameObject.Find("ChildPlayer").transform;
+        timer = 0;
+        fearLevel = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(timer < fearGainRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+            child.GetComponent<FearInfo>().fearLevel += fearGain;
+            fearLevel += fearGain;
+        }
+
+        if(fearLevel > shakeThreshold)
+        {
+            transform.position = new Vector3(Mathf.Sin(Time.time * shakeSpeed), transform.position.y, transform.position.z);
+        }
+
+        
+	}
+}
---
+++ Assets/FearInfo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FearInfo : MonoBehaviour {
+
+    public float fearLevel = 0;
+
+    bool isAlive = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(fearLevel > 100)
+        {
+            isAlive = false;
+        }
+
+
+	}
+}
