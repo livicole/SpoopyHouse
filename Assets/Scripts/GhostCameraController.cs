7d4662cfaa60f7af8312be888b054da9fcb9f302
Calvin So <ccs403@nyu.edu>
2016-09-14 10:30:50 -0400

    Panning the ghost camera should now work as well.

---
+++ Assets/Scripts/GhostCameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostCameraController : MonoBehaviour {
+
+    [SerializeField]
+    float panSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float inputX = Input.GetAxis("HorizontalCamera2");
+        float inputY = Input.GetAxis("VerticalCamera2");
+
+        Vector3 movementVector = new Vector3(inputX, 0, inputY);
+        movementVector = movementVector.normalized * panSpeed;
+	
+	}
+}
