dea9cadb251d5f27b2ecc5cc0be00ab9b587550c
Noah Salaway <nms348@nyu.edu>
2016-09-20 15:19:56 -0400

    Boolean placeholders

---
+++ Assets/Scripts/Booleans.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Booleans : MonoBehaviour {
+
+
+    public bool one, two, three, four, five, six, seven, eight, nine, ten;
+
+    public bool hasOne, hasTwo, hasThree, hasFour, hasFive, hasSix, hasSeven, hasEight, hasNine, hasTen;
+
+    public bool isOne, isTwo, isThree, isFour, isFive, isSix, isSeven, isEight, isNine, isTen;
+
+
+	// Use this for initialization
+	void Start () {
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (isOne)
+        {
+
+        }
+        if (isTwo)
+        {
+
+        }
+        if (isThree)
+        {
+
+        }
+        if (isFour)
+        {
+
+        }
+        if (isFive)
+        {
+
+        }
+        if (isSix)
+        {
+
+        }
+        if (isSeven)
+        {
+
+        }
+        if (isEight)
+        {
+
+        }
+        if (isNine)
+        {
+
+        }
+        if (isTen)
+        {
+
+        }
+        /////////////////////////////////////////
+
+        if (hasOne)
+        {
+
+        }
+        if (hasTwo)
+        {
+
+        }
+        if (hasThree)
+        {
+
+        }
+        if (hasFour)
+        {
+
+        }
+        if (hasFive)
+        {
+
+        }
+        if (hasSix)
+        {
+
+        }
+        if (hasSeven)
+        {
+
+        }
+        if (hasEight)
+        {
+
+        }
+        if (hasNine)
+        {
+
+        }
+        if (hasTen)
+        {
+
+        }
+
+        //////////////
+
+
+
+        if (one)
+        {
+
+        }
+        if (two)
+        {
+
+        }
+        if (three)
+        {
+
+        }
+        if (four)
+        {
+
+        }
+        if (five)
+        {
+
+        }
+        if (six)
+        {
+
+        }
+        if (seven)
+        {
+
+        }
+        if (eight)
+        {
+
+        }
+        if (nine)
+        {
+
+        }
+        if (ten)
+        {
+
+        }
+    }
+}
