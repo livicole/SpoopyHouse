4af7f0a2dfa6f8ab96f7a355c7a92e978e2023e2
Olivia Mignone <om651@nyu.edu>
2016-10-03 15:52:28 -0400

    drawer animation

---
+++ Assets/Scripts/Dresser.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dresser : MonoBehaviour {
+
+	private Animator dresserAnimator;
+
+	// Use this for initialization
+	void Start () {
+		dresserAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			dresserAnimator.SetBool ("DrawerIsOpen", true);
+			dresserAnimator.SetBool ("DrawerIsClosed", false);
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			dresserAnimator.SetBool ("DrawerIsClosed", true);
+			dresserAnimator.SetBool ("DrawerIsOpen", false);
+		}
+	
+	}
+}
