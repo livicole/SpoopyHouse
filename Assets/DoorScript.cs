275ba51943f317632413ed78918c2817725c7d1e
Noah Salaway <nms348@nyu.edu>
2016-10-17 09:41:36 -0400

    in class

--- Assets/DoorScript.cs
+++ Assets/DoorScript.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class DoorScript : MonoBehaviour {
 
+    public GameObject otherDoor;
+    public int priority;
+
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.name == "doubleDoor")
+        {
+            Debug.Log("found a door");
+            if (priority > col.GetComponentInParent<DoorScript>().priority)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void ReactivateDoor()
+    {
+        gameObject.SetActive(true);
+    }
 }
---
+++ Assets/DoorTracker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTracker : MonoBehaviour {
+
+    public GameObject[] doors = new GameObject[10];
+
+
+	// Use this for initialization
+	void Start () {
+        doors = GameObject.FindGameObjectsWithTag("Door");
+        for (int i=0; i<10; i++)
+        {
+            doors[i].GetComponent<DoorScript>().priority = i;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
