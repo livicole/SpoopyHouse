45ac0a1e3c74064daf36c4a28317f1e068d43359
Olivia Mignone <om651@nyu.edu>
2016-10-18 14:16:49 -0400

    noah fixed my sound yaya

--- Assets/Scripts/ChildScripts/Breathing.cs
+++ Assets/Scripts/ChildScripts/Breathing.cs
@@ -1,21 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Breathing : MonoBehaviour {
 
 	public AudioSource heavyBreathing;
+	bool hasBeenPlaying = false;
+
 
 	// Update is called once per frame
 	void Update () {
 		GameObject[] ghostToys = GameObject.FindGameObjectsWithTag ("Toys");
+		if (ghostToys.Length == 0) {
+			heavyBreathing.Stop ();
+			hasBeenPlaying = false;
+		}
 		foreach (GameObject target in ghostToys) {
+
 			float distance = Vector3.Distance (target.transform.position, transform.position);
+			Debug.Log (distance + " to " + target.name);
 			if (distance <= 20) {
-				heavyBreathing.enabled = true;
-				heavyBreathing.loop = true;
-			} else if (distance > 20) {
-				heavyBreathing.enabled = false;
-				heavyBreathing.loop = false;
+				if (!hasBeenPlaying) {
+					hasBeenPlaying = true;
+					heavyBreathing.Play ();
+				}
+			} else {
+				hasBeenPlaying = false;
+				heavyBreathing.Stop ();
 			}
 		}
 	}
