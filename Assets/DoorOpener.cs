b5040b53b3d49aaabfed1d0120b9355ddef5c61c
Noah Salaway <nms348@nyu.edu>
2016-11-15 16:19:52 -0500

    Doors open on button press, but are not fully implemented

---
+++ Assets/DoorOpener.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpener : MonoBehaviour {
+
+    bool open = false;
+    float angle;
+    Transform hinge;
+    GameObject child;
+
+    public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+        child = GameObject.Find("ChildPlayer");
+        hinge = transform.GetChild(0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(hinge.localEulerAngles.y);
+        
+        if (hinge.localEulerAngles.y > 95f)
+        {
+            angle = -0.1f;
+        }
+        else
+        {
+            angle = hinge.localEulerAngles.y;
+        }
+
+        if (Input.GetButtonDown("Ghost Button A"))
+        {
+
+            Ray rayToPlayer = new Ray(transform.position, child.transform.position - transform.position);
+            RaycastHit rayHitToPlayer = new RaycastHit();
+
+            if (Physics.Raycast(rayToPlayer, out rayHitToPlayer))
+            {
+                if (rayHitToPlayer.collider.name == "ChildPlayer" && Vector3.Distance(child.transform.position, transform.position) <= 2f)
+                open = !open;
+            }
+        }
+        //Debug.Log(hinge.localEulerAngles.y);
+ 
+
+        
+
+        if (open && angle < 90f) 
+        {
+            hinge.Rotate(0, speed * Time.deltaTime, 0);
+        }
+
+        if (!open && angle > 0f)
+        {
+            hinge.Rotate(0, -speed * Time.deltaTime, 0);
+        }
+
+    }
+}
--- Assets/Scripts/GridLocker.cs
+++ Assets/Scripts/GridLocker.cs
@@ -312,7 +312,7 @@ public class GridLocker : MonoBehaviour {
                 if (myDoor.tag == "Door")
                 {
                     //Debug.Log("Resetting: " + myDoor);
-                    myDoor.GetComponent<DoorScript>().ResetDoors();
+                    myDoor.GetComponent<DoorScript>().ResetDoor();
                 }
             }
         }
