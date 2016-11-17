e09bd2947d7bf93a1f7ec0c0b499957bce9ced2d
Olivia Mignone <om651@nyu.edu>
2016-11-17 16:17:47 -0500

    added cursor to drawers

--- Assets/Scripts/Dresser.cs
+++ Assets/Scripts/Dresser.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Dresser : MonoBehaviour
 {
@@ -8,12 +9,16 @@ public class Dresser : MonoBehaviour
 	Transform flashlight; 
 	public AudioSource soundManager;
 	public AudioClip drawerOpen, drawerClose;
+	Transform handCursor;
+
 
 	// Use this for initialization
 	void Start ()
 	{
+		handCursor = this.gameObject.GetComponent<Transform> ().Find ("Quad");
 		dresserAnimator = GetComponent<Animator> ();
         flashlight = GameObject.Find("Flashlight").transform;
+		handCursor.GetComponent<MeshRenderer> ().enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -22,11 +27,13 @@ public class Dresser : MonoBehaviour
         flashlight = GameObject.Find("Flashlight").transform;
         Ray drawerRay = new Ray (flashlight.position, flashlight.forward);
 		RaycastHit rayHitInfo = new RaycastHit ();
-		if (Physics.Raycast (drawerRay, out rayHitInfo, 1000f)) {
+		if (Physics.Raycast (drawerRay, out rayHitInfo, 5f)) {
 			//Debug.DrawRay (flaslight.position, flaslight.forward * 1000f, Color.blue);
 			//Debug.Log (rayHitInfo.collider.name);
-			if (rayHitInfo.collider.gameObject  == this.gameObject) {
-			//	Debug.Log ("it hit");
+			if (rayHitInfo.collider.gameObject == this.gameObject) {
+				handCursor.GetComponent<MeshRenderer> ().enabled = true;
+				handCursor.transform.LookAt (flashlight);
+				//	Debug.Log ("it hit");
 				if (Input.GetButtonDown ("Use")) {
 					dresserAnimator.SetTrigger ("Trigger");
 //					if (dresserAnimator.GetCurrentAnimatorStateInfo (0).IsName ("CloseDrawerIdle")) {
@@ -37,6 +44,8 @@ public class Dresser : MonoBehaviour
 //					}
 				}
 			}
+		} else {
+			handCursor.GetComponent<MeshRenderer> ().enabled = false;
 		}
 	}
 
