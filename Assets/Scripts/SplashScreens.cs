60cfa26f7e4fd76b830d6ad0c51d128c02f21707
Olivia Mignone <om651@nyu.edu>
2016-11-26 15:41:27 -0500

    added temp. splash screens

---
+++ Assets/Scripts/SplashScreens.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SplashScreens : MonoBehaviour {
+	
+	public void HitTutorial(){
+		SceneManager.LoadScene (1);
+	}
+
+	public void HitNoTutorial(){
+		SceneManager.LoadScene (4);
+	}
+}
