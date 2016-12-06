aeec61c1b0d6fe9574ce14e71e36bdb22dca448e
Olivia Mignone <om651@nyu.edu>
2016-12-06 03:08:29 -0500

    added more models & textures, redesigned title screen, temp title song

--- Assets/Scripts/SplashScreens.cs
+++ Assets/Scripts/SplashScreens.cs
@@ -8,34 +8,39 @@ public class SplashScreens : MonoBehaviour {
 	bool loadScene;
 
 	public Image loadingScreen;
+	public Button playButton;
+	public Button controlButton;
+	public Button creditButton;
 
 	[SerializeField]
 	private Text loadingText;
 
 	void Start(){
 		loadingScreen.enabled = false;
+		loadingText.enabled = false;
+		playButton.enabled = true;
+		controlButton.enabled = true;
+		creditButton.enabled = true;
 	}
 
 	void Update(){
 		if (loadScene == true) {
+			loadingText.enabled = true;
+			playButton.enabled = false;
+			controlButton.enabled = false;
+			creditButton.enabled = false;
 			loadingText.color = new Color (loadingText.color.r, loadingText.color.g, loadingText.color.b, Mathf.PingPong (Time.time, 1));
 		}
 	}
 
-	public void HitTutorial(){
-		SceneManager.LoadScene (1);
-	}
-
-	public void HitNoTutorial(){
-		loadScene = true;
-		loadingScreen.enabled = true;
-		loadingText.text = "LOADING";
+	public void PressedPlay(){
 		StartCoroutine (LoadGameScene ());
 	}
-
+		
 	IEnumerator LoadGameScene(){
+		loadScene = true;
 		yield return new WaitForSeconds (2);
-		AsyncOperation async = SceneManager.LoadSceneAsync (2);
+		AsyncOperation async = SceneManager.LoadSceneAsync (1);
 
 		while (!async.isDone) {
 			yield return null;
---
+++ Assets/Scripts/TitleScreenClock.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenClock : MonoBehaviour {
+
+	public float rotateAmount = 1.5f;
+	public AudioSource soundManager;
+	public AudioClip tickSound;
+	public AudioClip tockSound;
+
+	void Awake(){
+		StartCoroutine (TurnHand ());
+		StartCoroutine (ClockTicking ());
+	}
+
+	IEnumerator TurnHand ()
+	{
+		while (true) {
+			this.gameObject.transform.Rotate (0f, 0f, rotateAmount);
+			yield return new WaitForSeconds (1);
+		}
+	}
+
+	IEnumerator ClockTicking ()
+	{
+		while (true) {
+			//yield return new WaitForSeconds (1);
+			soundManager.PlayOneShot (tickSound, 0.4f);
+			yield return new WaitForSeconds (1);
+			soundManager.PlayOneShot (tockSound, 0.4f);
+			yield return new WaitForSeconds (1);
+		}
+	}
+}
